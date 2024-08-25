f4f550bc47e3595891aab56e824d5216bc97149a
roalli88 <ro_alli@protonmail.com>
2024-08-25 17:26:14 +0100

    Completed Activity 3

---
+++ python_projects/project_03_personal_information_card/personal_information_card.py
@@ -0,0 +1,12 @@
+name = "Carlos Santana"
+age = 44
+city = "Jalasca"
+hobby = "Playing the guitar"
+print("\t###################################")
+print("\t#    Personal Information Card    #")
+print("\t###################################")
+print(f"\tName: {name}")
+print(f"\tCity: {city}")
+print(f"\tAge : {age}")
+print(f"\thobby : {hobby}")
+
